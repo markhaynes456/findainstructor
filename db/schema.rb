4404026704de4cffd057cb8853f04fc61fcc8aff
Mark Haynes <markhaynes456@gmail.com>
2015-10-30 12:12:58 +0000

    config change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028120833) do
+ActiveRecord::Schema.define(version: 20151029121156) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -19,9 +19,29 @@ ActiveRecord::Schema.define(version: 20151028120833) do
     t.string   "email"
     t.string   "password"
     t.boolean  "instructor"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
     t.string   "password_digest"
+    t.text     "profile"
+    t.float    "first_pass_rate"
+    t.float    "second_pass_rate"
+    t.date     "started_teaching"
+    t.integer  "students_taught"
+    t.time     "working_hours_monday_start"
+    t.time     "working_hours_monday_end"
+    t.time     "working_hours_tuesday_start"
+    t.time     "working_hours_tuesday_end"
+    t.time     "working_hours_wednesday_start"
+    t.time     "working_hours_wednesday_end"
+    t.time     "working_hours_thursday_start"
+    t.time     "working_hours_thursday_end"
+    t.time     "working_hours_friday_start"
+    t.time     "working_hours_friday_end"
+    t.time     "working_hours_saturday_start"
+    t.time     "working_hours_saturday_end"
+    t.time     "working_hours_sunday_start"
+    t.time     "working_hours_sunday_end"
+    t.integer  "hourly_rate"
   end
 
 end
