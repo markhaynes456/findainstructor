2d62f9b39baf4d1229c399697a80c1db6b333500
Mark Haynes <markhaynes456@gmail.com>
2015-11-06 14:21:46 +0000

    improved psql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,15 @@
 
 ActiveRecord::Schema.define(version: 20151029121156) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "postsqls", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
